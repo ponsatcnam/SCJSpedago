2beefcc6cad204c3d4993f48c19a7343c12e6f50
Jean-Ferdy <jean-ferdinand.susini@cnam.fr>
2023-12-18 15:11:47 +0100

    [jfs] version actuelle

--- VJS/src/transfo.js
+++ VJS/src/transfo.js
@@ -4,10 +4,51 @@ fs.readFile('semantics.js', 'utf8', function(err, data){
   const comments=data.match(/\/\*(?:.|\n)*?\*\//gm);
   const lignes=comments.join("").split("\n");
   const rules=[];
+  const activ_cases=[];
+  const eoi_cases=[];
+  const instant_cases=[];
   for(var i=0; i <lignes.length; i++){
     if(/----/.test(lignes[i])){
-      rules.push({ hyp: lignes[i-1], conc: lignes[i+1] });
+      const hyp=lignes[i-1].replace(/(^\s*)|(\s*$)/, '');
+      const conc=lignes[i+1].replace(/(^\s*)|(\s*$)/, '');
+      const rule={ hyp: hyp.split(/\s*;\s*/g), conc: conc.split(/\s*->\s*/g) };
+      rules.push(rule);
+      if(rule.conc[0].startsWith("activ")){
+	const init=rule.conc[0];
+	const inst=init.substring(init.indexOf("(")+1, init.length-1);
+	console.log(inst);
+	activ_cases.push(rule);
+	}
+      if(rule.conc[0].startsWith("eoi")){
+	eoi_cases.push(rule);
+	}
+      if(rule.conc[0].startsWith("instant")){
+	instant_cases.push(rule);
+	}
       }
     }
-    console.log(rules);
+  console.log(`
+/* return a term */
+function Result(term, E){
+  this.term=term;
+  this.E=E;
+  }
+function react(p){
+  return instant(p, {});
+  }
+function instant(term, E){
+  return ativ(new Close(p), E).term;
+  }
+function activ(term, E){
+  const head=term.substr(0, term.idexOf("("));
+  switch(head){
+    `);
+  for(var rl of activ_cases){
+    //console.log(rl.hyp);
+    }
+  //console.log('eoi:', eoi_cases);
+  console.log(`
+    default: throw new Error("unknown rule");
+    }
+  }`);
   });
